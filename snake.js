986d731f26ac5615cc50603bebd71c943e26de2d
Askansz <122436444+Askansz@users.noreply.github.com>
2023-10-05 11:35:40 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -29,7 +29,7 @@ window.onload = function() {
 
     placeFood();
     document.addEventListener("keyup", changeDirection);
-    setInterval(update, 1000/10); //100 milliseconds
+    setInterval(update, 1000/5); //200 milliseconds
 }
 
 function update() {
